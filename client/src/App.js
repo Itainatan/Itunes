a6824c45c28b9b04609c4f6366da089e697ce922
Ranchuk <rannnz1@gmail.com>
2019-06-19 00:08:10 +0300

    enable core policys

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      server_respond: "NO_server_respond"
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000")
+      .then(res => {
+        console.log(res);
+        this.setState({ server_respond: res.data });
+      })
+      .catch(err => console.log("error respond"));
+  }
+  render() {
+    return <h1>{this.state.server_respond}</h1>;
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 
 const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req, res) => res.send("success to GET from server"));
 

f02f002b8398d61c098e2552ec6f94df4b40fa87
Ranchuk <rannnz1@gmail.com>
2019-06-28 19:45:57 +0300

    sssssss

--- client/src/App.js
+++ client/src/App.js
@@ -9,15 +9,17 @@ class App extends Component {
     };
   }
   componentDidMount() {
-    axios
-      .post("/api/users/current", { email: "rannn03@walla.co.il" })
-      .then(res => {
-        console.log(res);
-        this.setState({ server_respond: res.data.name });
-      })
-      .catch(err => {
-        // this.setState({ server_respond: err });
-      });
+    setTimeout(() => {
+      axios
+        .post("/api/users/current", { email: "rannn03@walla.co.il" })
+        .then(res => {
+          console.log(res);
+          this.setState({ server_respond: res.data.name });
+        })
+        .catch(err => {
+          // this.setState({ server_respond: err });
+        });
+    }, 3000);
   }
   render() {
     return <h1>{this.state.server_respond}</h1>;

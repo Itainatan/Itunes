71cd3d7637852b1572006b7be0210363702a25fe
Ranchuk <rannnz1@gmail.com>
2019-06-30 07:34:08 +0300

    add hook

--- client/src/components/dashbaord.js
+++ client/src/components/dashbaord.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Dashboard = () => {
-  return <h1>Dashboard Page</h1>;
+  const [name, setName] = useState("Harry");
+  const [surename, setSureName] = useState("potter");
+
+  const handleNameChange = e => {
+    setName(e.target.value);
+  };
+  const handleSureChange = e => {
+    setSureName(e.target.value);
+  };
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+      <div>
+        <input value={name} onChange={handleNameChange} />
+      </div>
+      <div>
+        <input value={surename} onChange={handleSureChange} />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;

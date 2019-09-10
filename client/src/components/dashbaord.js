513ddc8914c3e4ca57071179381df21d5102fd50
Ranchuk <rannnz1@gmail.com>
2019-09-10 22:38:57 +0300

    remove capctha and unnecessary code

--- client/src/components/dashbaord.js
+++ client/src/components/dashbaord.js
@@ -1,22 +1,8 @@
 import React, { Component } from "react";
-import { withGoogleReCaptcha } from "react-google-recaptcha-v3";
-import axios from "axios";
 import { Link } from "react-router-dom";
 
 class Dashboard extends Component {
-  async componentDidMount() {
-    const token = await this.props.googleReCaptchaProps.executeRecaptcha(
-      "homepage"
-    );
-    axios
-      .post("/api/auth/captcha", { token })
-      .then(res => {
-        console.log(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+
   render() {
     return (
       <div>
@@ -29,32 +15,4 @@ class Dashboard extends Component {
   }
 }
 
-export default withGoogleReCaptcha(Dashboard);
-
-//HOOKS EXAMPLE
-
-// const Dashboard = async props => {
-//   const [name, setName] = useState("Harry");
-//   const [surename, setSureName] = useState("potter");
-//   console.log(this.props);
-//   const token = await this.props.googleReCaptchaProps.executeRecaptcha(
-//     "homepage"
-//   );
-//   const handleNameChange = e => {
-//     setName(e.target.value);
-//   };
-//   const handleSureChange = e => {
-//     setSureName(e.target.value);
-//   };
-//   return (
-//     <div>
-//       <h1>Dashboard Page</h1>
-//       <div>
-//         <input value={name} onChange={handleNameChange} />
-//       </div>
-//       <div>
-//         <input value={surename} onChange={handleSureChange} />
-//       </div>
-//     </div>
-//   );
-// };
+export default Dashboard;
